9edb1e9e564cfb299c9cf5250292bdb90a7e6fdf
Eric Alves <ericalves9999@gmail.com>
2022-03-12 20:22:04 +0100

    1 commit

---
+++ app/src/main/java/com/example/alvericpractica3/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.alvericpractica3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
